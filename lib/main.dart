e32d75270b1bd4e639f4aae48da3919980e70ba0
Moksh Bhansali <60219805+MokshBhansali@users.noreply.github.com>
2023-07-24 16:14:37 +0530

    Face Detection using ML
    Face Detection using ML

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:face_detection_app/screens/splash.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Face Detector App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
